93252f0176e6dc09045c09522ea9db24a1daf1a6
egdod333 <egdodlikerperlinnoise@gmail.com>
2026-03-15 19:59:21 +0000

    big asset loading progress idk why it doesn't work yet

--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <type_traits>
-#include <cmath>
 FILE* debug=fopen("./debug/debug.log","w");
 bool logmisc=false;
 template<typename T> concept arith=std::is_arithmetic_v<T>;
@@ -26,15 +25,12 @@ template<comp T,comp...U> T constexpr max(T t, U...a){
 #include <assets.hpp>
 int main() {
   puts("\rRAT GAME 16");
-  puts("LOADING MODELS");
+  puts("Loading assets...");
   assets::asset3d_t model=assets::readAsset3d("assets/cube.rgmdl");
   gui::init();
   unsigned char escapes=0;
   unsigned char mode=0;
   unsigned char modes=2;
-  unsigned char rotamnt=16;
-  float rotamntrad = (rotamnt/128.0f)*M_PI;
-  float rottrck=0;
   while(true){
     char c=gui::readInput();
     switch(c){//escapey bits. add more later probably. note that tmux is doing strange things to us
@@ -46,19 +42,17 @@ int main() {
       switch(c){
         case 'A':mode=(mode+1)%modes;break;//up
         case 'B':mode=(mode+modes-1)%modes;break;//down
-        case 'C':mesh::camera_rotation.z-=rotamnt;rottrck+=rotamntrad;break;//left
-        case 'D':mesh::camera_rotation.z+=rotamnt;rottrck-=rotamntrad;break;//right
-        if(rottrck > 2*M_PI){rottrck-=2*M_PI;}
-        if(rottrck < 2*M_PI){rottrck+=2*M_PI;}
+        case 'C':mesh::camera_rotation.z-=16;break;//left
+        case 'D':mesh::camera_rotation.z+=16;break;//right
       }
       escapes=0;
       // continue;
     }else{
       switch(c){
-        case 'w':mesh::camera_position.x+=cos(rottrck);mesh::camera_position.y+=sin(rottrck);break;
-        case 's':mesh::camera_position.x-=cos(rottrck);mesh::camera_position.y-=sin(rottrck);break;
-        case 'd':mesh::camera_position.x-=sin(rottrck);mesh::camera_position.y+=cos(rottrck);break;
-        case 'a':mesh::camera_position.x+=sin(rottrck);mesh::camera_position.y-=cos(rottrck);break;
+        case 'w':mesh::camera_position.x+=1;break;
+        case 's':mesh::camera_position.x-=1;break;
+        case 'd':mesh::camera_position.y+=1;break;
+        case 'a':mesh::camera_position.y-=1;break;
         case 'x':{
           gui::clear_scr();
           for(short unsigned int i=0;i<model.mesh.tricount;i++){
